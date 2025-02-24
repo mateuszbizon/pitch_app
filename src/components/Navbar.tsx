d3c91835caa4d9ccaaa64c22870d64be51d5e11e
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-02-24 12:00:13 +0100

    added navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,49 @@
+import { auth, signIn, signOut } from '@/auth'
+import Link from 'next/link'
 import React from 'react'
 
-function Navbar() {
+async function Navbar() {
+    const session = await auth()
+
   return (
-    <div>Navbar</div>
+    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+        <nav className='flex justify-between items-center'>
+            <Link href={"/"}>
+                <span className='text-black text-xl'><span className='text-red-500'>YC</span>Directory</span>
+            </Link>
+            <div className='flex items-center gap-5 text-black'>
+                {session && session.user ? (
+                    <>
+                        <Link href={"/startup/create"}>
+                            Create
+                        </Link>
+                        <form action={async () => {
+                            "use server"
+
+                            await signOut({ redirectTo: "/" })
+                        }}>
+                            <button type='submit'>
+                                Logout
+                            </button>
+                        </form>
+                        <Link href={`/user/${session.user.id}`}>
+                            {session.user.name}
+                        </Link>
+                    </>
+                ) : (
+                    <form action={async () => {
+                        "use server"
+
+                        await signIn("github")
+                    }}>
+                        <button type='submit'>
+                            Login
+                        </button>
+                    </form>
+                )}
+            </div>
+        </nav>
+    </header>
   )
 }
 
