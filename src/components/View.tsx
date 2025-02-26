fc56f8ec35228ff34c6b13920b537ed0db989841
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-02-26 19:03:53 +0100

    updating startup views

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Ping from './Ping';
 import { client } from '@/sanity/lib/client';
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries';
+import { writeClient } from '@/sanity/lib/writeClient';
+import { after } from "next/server"
 
 type ViewProps = {
     id: string;
@@ -9,6 +11,10 @@ type ViewProps = {
 
 async function View({ id }: ViewProps) {
     const { views: totalViews } = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id })
+
+    after(async () => {
+        return await writeClient.patch(id).set({ views: totalViews + 1 }).commit()
+    })
 
   return (
     <div className='view-container'>
