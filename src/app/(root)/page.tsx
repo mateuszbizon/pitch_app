aa6f6062f0fb00b265340b3e02b1132caa499bb0
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-02-27 11:28:59 +0100

    connect next auth with sanity

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
@@ -7,6 +8,9 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
     const query = (await searchParams).query
     const params = { search: query || null }
     const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
+
+    const session = await auth()
+    console.log(session)
 
   return (
     <>
